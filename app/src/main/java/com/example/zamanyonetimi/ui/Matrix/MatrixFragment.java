f5d24dd870033082c697060c52ccb585d5ac3bca
arzuyb <arzuyucekaya@gmail.com>
2020-06-30 23:00:02 +0300

    matrix

--- app/src/main/java/com/example/zamanyonetimi/ui/Matrix/MatrixFragment.java
+++ app/src/main/java/com/example/zamanyonetimi/ui/Matrix/MatrixFragment.java
@@ -1,5 +1,6 @@
 package com.example.zamanyonetimi.ui.Matrix;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,13 +10,17 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.zamanyonetimi.R;
+import com.example.zamanyonetimi.ui.Inbox.InboxFragment;
 
 public class MatrixFragment extends Fragment {
-
+    RecyclerView recyclerView2, recyclerView4, recyclerView5, recyclerView6;
+    View root;
     private MatrixViewModel homeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -28,8 +33,49 @@ public class MatrixFragment extends Fragment {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
+                tanimla();
+                recyclerView2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(getContext(), InboxFragment.class);
+                        startActivity(intent);
+                    }
+
+                });
+                recyclerView4.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(getContext(), InboxFragment.class);
+                        startActivity(intent);
+                    }
+                });
+                recyclerView5.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(getContext(), InboxFragment.class);
+                        startActivity(intent);
+                    }
+                });
+                recyclerView6.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(getContext(), InboxFragment.class);
+                        startActivity(intent);
+                    }
+                });
             }
         });
+
         return root;
     }
+
+    public void tanimla() {
+        recyclerView2 = root.findViewById(R.id.recyclerView2);
+        recyclerView4 = root.findViewById(R.id.recyclerView4);
+        recyclerView5 = root.findViewById(R.id.recyclerView5);
+        recyclerView6 = root.findViewById(R.id.recyclerView6);
+
+    }
+
+
 }
